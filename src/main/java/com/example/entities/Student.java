ddcf70caa0e7e6649a80476de43c6a7828adc0d9
Sincal <sincaljulio200@gmail.com>
2023-06-16 10:59:50 -0600

    Todo bien hasta ahora

--- src/main/java/com/example/entities/Student.java
+++ src/main/java/com/example/entities/Student.java
@@ -13,7 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 @Table(name = "student")
